f7dc6b263d73505af25082b72be48fca11a09953
lespend <lespend33@gmail.com>
2024-03-19 17:17:22 +0500

    add dark mode and shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
@@ -18,8 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
-        {children}
+        <ThemeProvider
+          defaultTheme="system"
+          attribute="class"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header/>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { FiHome } from "react-icons/fi";
 import { FiInfo } from "react-icons/fi";
 import MenuItem from "./MenuItem";
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 const Header = () => {
   return (
@@ -10,12 +11,15 @@ const Header = () => {
         <MenuItem to="/" Icon={<FiHome/>} title="Главная"/>
         <MenuItem to="/about" Icon={<FiInfo/>} title="О нас"/>
       </nav>
-      <Link className="text-3xl" href="/">
-        Movie
-        <strong className="font-bold bg-green-600 px-2 rounded-lg text-white ml-1">
-          Quest
-        </strong>
-      </Link>
+      <div className="flex gap-4 items-center">
+        <Link className="text-3xl" href="/">
+          Movie
+          <strong className="font-bold bg-green-600 px-2 rounded-lg text-white ml-1">
+            Quest
+          </strong>
+        </Link>
+        <DarkModeSwitch/>
+      </div>
     </header>
   );
 };
